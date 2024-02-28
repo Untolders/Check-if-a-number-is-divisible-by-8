bf59bb170cad4e4d818745dc08ded0a711510078
Anurag Singh <131255604+Untolders@users.noreply.github.com>
2024-02-28 20:01:48 +0530

    Create Check-if-a-number-is-divisible-by-8.cpp

---
+++ Check-if-a-number-is-divisible-by-8.cpp
@@ -0,0 +1,83 @@
+#################################################################################  Question  ###################################################################################################################################
+
+
+
+Given a string representation of a decimal number s, check whether it is divisible by 8.
+
+Example 1:
+
+Input:
+s = "16"
+Output:
+1
+Explanation:
+The given number is divisible by 8,
+so the driver code prints 1 as the output.
+Example 2:
+
+Input:
+s = "54141111648421214584416464555"
+Output:
+-1
+Explanation:
+Given Number is not divisible by 8, 
+so the driver code prints -1 as the output.
+Your Task:
+You don't need to read input or print anything.Your task is to complete the function DivisibleByEight() which takes a string s as the input and returns 1 if the number is divisible by 8, else it returns -1.
+
+Expected Time Complexity: O(1).
+Expected Auxillary Space: O(1).
+
+Constraints:
+1 <= |s| <= 106
+
+
+
+
+
+
+
+
+
+################################################################################  Solution  ####################################################################################################################################
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    int DivisibleByEight(string s){
+        
+        int num=0;
+        int n=s.size();
+        int j=1;
+        for(int i=n-1;i>=n-4 && i>=0;i--){
+            num=num+(s[i]-'0')*j;
+            j*=10;
+        }
+        if(num==0 ||num%8==0)
+        return 1;
+        
+        return -1;
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string S;
+        cin>>S;
+        Solution ob;
+        cout<<ob.DivisibleByEight(S)<<"\n";
+    }
+}
+// } Driver Code Ends
